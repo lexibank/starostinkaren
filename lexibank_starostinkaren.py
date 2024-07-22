eed56b62db4a692949fe08d7c11fe4f2bdb969f8
lingulist <mattis.list@lingpy.org>
2024-07-22 14:16:18 +0200

    adjust the raw data

--- lexibank_starostinkaren.py
+++ lexibank_starostinkaren.py
@@ -5,6 +5,6 @@ from pylexibank.providers import tob
 class Dataset(tob.TOB):
     dir = Path(__file__).parent
     id = "starostinkaren"
-    pages = 1
+    pages = 10
     name = "krn"
     dset = "stb"
