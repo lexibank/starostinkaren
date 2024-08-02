9a62e201aa5c8dd21ea94d305c1004b61f836e61
chrzyki <christoph@functor-argument.net>
2024-08-02 12:19:59 +0200

    Prepare release, fix FormSpec

--- CONTRIBUTORS.md
+++ CONTRIBUTORS.md
@@ -3,5 +3,6 @@
 Name | GitHub user | Description | Role
  --- | --- | --- | ---
 Robert Forkel | @xrotwang | initial code | Other
-Christoph Rzymski | @chrzyki | patron | Other
-George Starostin | | data collection | Author
+Christoph Rzymski | @chrzyki | patron | Editor
+Johann-Mattis List | @LinguList | code, profile, maintainer | Editor
+George S. Starostin | | data collection | Author, DataCurator
--- lexibank_starostinkaren.py
+++ lexibank_starostinkaren.py
@@ -1,8 +1,18 @@
 from pathlib import Path
 from pylexibank.providers import tob
 
+from pylexibank.forms import FormSpec
+
 
 class Dataset(tob.TOB):
+    form_spec = FormSpec(
+        brackets={"(": ")"},
+        separators=";/,~",
+        missing_data=("?", "-", ""),
+        replacements=[(" #", "")],
+        strip_inside_brackets=True,
+    )
+
     dir = Path(__file__).parent
     id = "starostinkaren"
     pages = 10
